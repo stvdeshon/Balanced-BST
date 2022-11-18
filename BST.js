bed15d553fce422b31d96baef0e1768d589563a0
stvdeshon <stvdeshon@gmail.com>
2022-11-18 16:13:43 -0700

    Upload initial files

---
+++ BST.js
@@ -0,0 +1,29 @@
+import mergeSort from "./merge-sort.js";
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  buildTree(array) {
+    mergeSort(array);
+  }
+
+  prettyPrint(node, prefix = "", isLeft = true) {
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+}
